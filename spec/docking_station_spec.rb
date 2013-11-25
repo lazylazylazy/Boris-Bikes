c2a99c2e12b30736ae3ebeb21fdaf0affff30329
Michael <michaelnicholasfisher@gmail.com>
2013-11-25 16:25:18 +0000

    fleshes out DockingStation class with methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -20,4 +20,8 @@ describe DockingStation do
 		expect(station).to be_full
 	end
 
+it 'should not accept a bike if it is full' do
+	20.times { station.dock(Bike.new) }
+	expect(lambda { station.dock(bike)}).to raise_error(RuntimeError)
+
 end
