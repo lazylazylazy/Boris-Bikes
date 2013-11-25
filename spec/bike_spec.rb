4aaf58479de8376883a6e1e2702bb952332d3913
Michael <michaelnicholasfisher@gmail.com>
2013-11-25 15:19:30 +0000

    first refactor

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,17 +1,16 @@
 require_relative '../lib/bike'
 
-describe Bike do 
+describe Bike do
+
+	let(:bike) { Bike.new } 
 	it 'should not be broken after we create it' do
-		bike = Bike.new
 		expect(bike).not_to be_broken
 	end
 	it 'should be able to break' do
-		bike = Bike.new
 		bike.break
 		expect(bike).to be_broken
 	end
 	it 'should be able to get fixed' do
-		bike = Bike.new
 		bike.break
 		bike.fix
 		expect(bike).not_to be_broken
