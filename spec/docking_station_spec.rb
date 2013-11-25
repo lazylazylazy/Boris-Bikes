9ca26f22dcba7ded8fcb3b234dc1ffaaa0d23ee3
Michael <michaelnicholasfisher@gmail.com>
2013-11-25 16:23:14 +0000

    fleshes out DockingStation class with methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/docking_station'
 
 describe DockingStation do
 	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new }
+	let(:station) { DockingStation.new(:capacity => 20) }
 	it 'should accept a bike' do
 	expect(station.bike_count).to eq(0)
 	station.dock(bike)
@@ -13,6 +13,11 @@ describe DockingStation do
 		station.dock(bike)
 		station.release(bike)
 		expect(station.bike_count).to eq(0)
+	end
+	it 'should know when it is full' do
+		expect(station).not_to be_full
+		20.times { station.dock(Bike.new) }
+		expect(station).to be_full
 	end
 
 end
